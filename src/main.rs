ac4cf8dc2423892f31bdadcce6a2d24f7f01b882
carlca <carl.caulkett@gmail.com>
2024-03-15 15:51:12 +0000

    First commit.

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use leptos::*;
+
+fn main() {
+  leptos::mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+  let (count, set_count) = create_signal(0);
+
+  view! {
+    <button on:click=move |_| {
+        set_count(3);
+    }>"Click me: " {count}</button>
+  }
+}
